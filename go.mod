8a7f8a4c916f1204b217b5b391df683c235c0b56
Cody Theroux <cody.theroux3@gmail.com>
2023-11-22 22:16:07 -0500

    added gitignore, set environment variables, started main package and loaded our port

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/codytheroux96/rss-aggregator
+
+go 1.21.3
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
+}
