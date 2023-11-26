d325c902f96603de1c8b138ff325634dda20268f
Cody Theroux <cody.theroux3@gmail.com>
2023-11-26 15:00:35 -0500

    allowing for creation and retrieval of feeds

--- handler_user.go
+++ handler_user.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-	"github.com/codytheroux96/rss-aggregator/internal/auth"
 	"github.com/codytheroux96/rss-aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -34,22 +33,9 @@ func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-
 	respondWithJSON(w, 201, databaseUserToUser(user))
 }
 
-func (apiCfg *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
-		return
-	}
-
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Could not get user: %v", err))
-		return
-	}
-
+func (apiCfg *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
